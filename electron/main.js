ab99db24869b137c120801286630a6d32edc8a88
Hammad Khan <hamdev73@gmail.com>
2026-06-16 00:57:38 +0500

    added build

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,9 @@
 import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -6,9 +11,13 @@ function createWindow() {
     height: 800,
   });
 
-  win.loadURL("http://localhost:5173/");
+  const isDev = !app.isPackaged;
+
+  if (isDev) {
+    win.loadURL("http://localhost:5173");
+  } else {
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
 }
 
-app.whenReady().then(() => {
-  createWindow();
-});
+app.whenReady().then(createWindow);
